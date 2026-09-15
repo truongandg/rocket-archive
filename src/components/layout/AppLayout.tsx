6d1583752356969f7dba1c88bfc7651bedd1ad19
An Duong <truongan106@gmail.com>
2026-09-15 21:41:43 +0700

    chore: scroll to top on navigation

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,10 +1,17 @@
-import type { ReactNode } from "react";
+import { useLayoutEffect, type ReactNode } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "./Header";
 
 interface AppLayoutProps {
   children: ReactNode;
 }
 export default function AppLayout({ children }: AppLayoutProps) {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [pathname]);
+
   return (
     <div className="min-h-screen bg-black text-white">
       <Header />
